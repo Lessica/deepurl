7b52810d3f6900e63b37fc7fe799626ec3d24ec2
Lessica <i.82@me.com>
2018-02-04 13:31:37 +0800

    Pass GET arguments

--- URLShortener/settings.py
+++ URLShortener/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=km&^^1uor=pqj+pkbyxy%)orc8o=in+*h(bi^lh^y7ivod#zb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['82flex.com', '127.0.0.1']
 
